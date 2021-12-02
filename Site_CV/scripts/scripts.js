f6bd74c858653b9ca1c07ce57ef6e86e6f8eb8c5
CaroCap <carolinecap.event@gmail.com>
2021-12-02 19:07:06 +0100

    Modal barre brune et video

--- Site_CV/scripts/scripts.js
+++ Site_CV/scripts/scripts.js
@@ -11,28 +11,65 @@ document.getElementById("ageCaroModal").innerHTML = getAge(BIRTHDATE_CARO) + " a
 
 
 // MODALS
-// Get the modal
-var modal = document.getElementById("decouvrirPlus");
-// ! Récupérer le nom de l'id du click pour pouvoir le mettre dans le modal et ouvrir le bon modal
-
-// Get the button that opens the modal
-var btn = document.getElementById("modalDecouvrirPlus");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.addEventListener("click", ouvrirModal);
-function ouvrirModal(event){
+//CHOIX COULEURS JOUEUR
+choixCouleur = document.getElementsByClassName('couleur');
+for (let indexCouleur = 0; indexCouleur < choixCouleur.length; indexCouleur++) {
+    choixCouleur[indexCouleur].addEventListener("click", pickColor);
+}
+function pickColor(event){
     event.preventDefault();
-    modal.style.display = "block";
+    let colorPicked = event.currentTarget.innerText;
+    for (let index = 0; index < liste_couleurs.length; index++) {
+        if(document.getElementById('codeJoueur').value.length<8){
+            if(colorPicked == liste_couleurs[index]){
+                document.getElementById('codeJoueur').value += (liste_couleurs[index]);
+            }
+        }      
+    }
+    //replacer le curseur dans le input pour l'entrée suivante
+    document.getElementById("codeJoueur").focus();
 }
 
+//TABLEAU DES MODALS
+let choixModals = document.getElementsByClassName('modalClic');
+for (let indexModal = 0; indexModal < choixModals.length; indexModal++) {
+    choixModals[indexModal].addEventListener("click", ouvrirModal);
+}
+// function openModal(event) {
+//     event.preventDefault();
+//     let modalID = this.id;
+//     for (let index = 0; index < choixModals.length; index++) {
+//         if(document.getElementById('codeJoueur').value.length<8){
+//             if(modalPicked == choixModals[index]){
+//                 document.getElementById('codeJoueur').value += (choixModals[index]);
+//             }
+//         }      
+//     }
+// }
+
+// Get the modal
+let modal;
+let modalID = "";
+// ! Récupérer le nom de l'id du click pour pouvoir le mettre dans le modal et ouvrir le bon modal
+// Get the button that opens the modal
+// var btn = document.getElementById("decouvrirPlus");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+// btn.addEventListener("click", ouvrirModal);
+
+function ouvrirModal(event){
+    event.preventDefault();
+    modalID = "modal"+this.id;
+console.log("ID Modal", modalID); // ! A SUPPRIMER
+    modal = document.getElementById(modalID);
+console.log("Modal", modal); // ! A SUPPRIMER
+    modal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
